0748dc5eaccb41132dd45dc496c25ded2de8f2ec
juytj21 <juytj21@gmail.com>
2024-08-03 17:57:25 +0900

    마이페이지 프로필 수정

--- screens/MyPageProfile.jsx
+++ screens/MyPageProfile.jsx
@@ -11,20 +11,10 @@ const MyPageProfile = () => {
     return (
         <View style={styles.mypageProfile}>
             <MyPageTopNav/>
-            {/*<View style={[styles.topNav, styles.textPosition1]}>*/}
-            {/*    <View style={[styles.topNavContentFrame, styles.mppFNicknameFlexBox]}>*/}
-            {/*        <Image*/}
-            {/*            style={styles.mppPFrameIcon}*/}
-            {/*            contentFit="cover"*/}
-            {/*            source={require("../assets/arrow_back.png")}*/}
-            {/*        />*/}
-            {/*        <Text style={styles.plannie}>Plannie</Text>*/}
-            {/*    </View>*/}
-            {/*</View>*/}
             <View style={styles.mppContentBox}>
                 <View style={styles.mppContent}>
                     <View style={styles.mppGNickname}>
-                        <Text style={[styles.text, styles.textPosition1]}>닉네임</Text>
+                        <Text style={[styles.text, styles.textPosition]}>닉네임</Text>
                         <View style={[styles.mppLine, styles.mppPosition]} />
                         <View style={[styles.mppFNickname, styles.mppFNicknameFlexBox]}>
                             <Text style={styles.textTypo}>미션</Text>
@@ -32,7 +22,7 @@ const MyPageProfile = () => {
                                 <Image
                                     style={styles.mppInitializationIcon}
                                     contentFit="cover"
-                                    // source={require("../assets/mpp-initialization.png")}
+                                    source={require("../assets/MPP_Initialization.png")}
                                 />
                                 <Text style={[styles.mppNicknameNum, styles.bodyTextTypo]}>
                                     2/20
@@ -41,31 +31,31 @@ const MyPageProfile = () => {
                         </View>
                     </View>
                     <View style={[styles.mppGName, styles.mppSpaceBlock]}>
-                        <Text style={[styles.text, styles.textPosition1]}>이름</Text>
+                        <Text style={[styles.text, styles.textPosition]}>이름</Text>
                         <View style={[styles.mppLine, styles.mppPosition]} />
                         <Text style={[styles.text3, styles.textTypo]}>문박박장</Text>
                     </View>
                     <View style={[styles.mppGName, styles.mppSpaceBlock]}>
-                        <Text style={[styles.text, styles.textPosition1]}>휴대폰</Text>
+                        <Text style={[styles.text, styles.textPosition]}>휴대폰</Text>
                         <View style={[styles.mppLine, styles.mppPosition]} />
                         <Text style={[styles.text3, styles.textTypo]}>010-1234-5678</Text>
                     </View>
                     <View style={[styles.mppGName, styles.mppSpaceBlock]}>
-                        <Text style={[styles.text, styles.textPosition1]}>이메일</Text>
+                        <Text style={[styles.text, styles.textPosition]}>이메일</Text>
                         <View style={[styles.mppLine, styles.mppPosition]} />
                         <Text style={[styles.text3, styles.textTypo]}>
                             missionkinggu@gmail.com
                         </Text>
                     </View>
                     <View style={[styles.mppGBirth, styles.mppSpaceBlock]}>
+                        <Text style={[styles.text7, styles.textPosition]}>생일</Text>
+                        <View style={[styles.mppLineShort, styles.mppPosition]} />
+                        <Text style={[styles.text8, styles.textTypo]}>2002/02/01</Text>
                         <Image
                             style={styles.arrowRightIcon}
                             contentFit="cover"
                             source={require("../assets/arrow_front.png")}
                         />
-                        <Text style={[styles.text7, styles.textPosition1]}>생일</Text>
-                        <View style={[styles.mppLineShort, styles.mppPosition]} />
-                        <Text style={[styles.text8, styles.textTypo]}>2002/02/01</Text>
                     </View>
                     <View style={[styles.mppGBirth, styles.mppSpaceBlock]}>
                         <Image
@@ -73,7 +63,7 @@ const MyPageProfile = () => {
                             contentFit="cover"
                             source={require("../assets/arrow_front.png")}
                         />
-                        <Text style={[styles.text7, styles.textPosition1]}>주소</Text>
+                        <Text style={[styles.text7, styles.textPosition]}>주소</Text>
                         <View style={[styles.mppLineShort, styles.mppPosition]} />
                         <Text style={[styles.text8, styles.textTypo]}>
                             대한민국, 수원시
@@ -85,7 +75,7 @@ const MyPageProfile = () => {
                             contentFit="cover"
                             source={require("../assets/arrow_front.png")}
                         />
-                        <Text style={[styles.text7, styles.textPosition1]}>성별</Text>
+                        <Text style={[styles.text7, styles.textPosition]}>성별</Text>
                         <View style={[styles.mppLineShort, styles.mppPosition]} />
                         <Text style={[styles.text8, styles.textTypo]}>미입력</Text>
                     </View>
@@ -95,39 +85,9 @@ const MyPageProfile = () => {
                 <Image
                     style={styles.mppPFrameIcon}
                     contentFit="cover"
-                    // source={require("../assets/mpp-p-frame.png")}
+                    source={require("../assets/MPP_P_frame.png")}
                 />
             </View>
-            {/*<View style={[styles.bottomNav, styles.tooltipBorder]}>*/}
-            {/*    <View style={styles.nav}>*/}
-            {/*        <View style={[styles.tooltip, styles.tooltipBorder]}>*/}
-            {/*            <Image*/}
-            {/*                style={[styles.beakIcon, styles.beakIconPosition]}*/}
-            {/*                contentFit="cover"*/}
-            {/*                // source={require("../assets/beak1.png")}*/}
-            {/*            />*/}
-            {/*            <Image*/}
-            {/*                style={[styles.beakStrokeIcon, styles.beakIconPosition]}*/}
-            {/*                contentFit="cover"*/}
-            {/*                // source={require("../assets/beak-stroke1.png")}*/}
-            {/*            />*/}
-            {/*            <Text style={[styles.title, styles.titleFlexBox]} />*/}
-            {/*            <Text*/}
-            {/*                style={[styles.bodyText, styles.bodyTextTypo]}*/}
-            {/*            >{`          `}</Text>*/}
-            {/*        </View>*/}
-            {/*        <Image*/}
-            {/*            style={styles.iconLayout}*/}
-            {/*            contentFit="cover"*/}
-            {/*            source={require("../assets/Home.png")}*/}
-            {/*        />*/}
-            {/*        <Image*/}
-            {/*            style={[styles.userIcon, styles.iconLayout]}*/}
-            {/*            contentFit="cover"*/}
-            {/*            source={require("../assets/User.png")}*/}
-            {/*        />*/}
-            {/*    </View>*/}
-            {/*</View>*/}
             <BottomNav/>
         </View>
     );
